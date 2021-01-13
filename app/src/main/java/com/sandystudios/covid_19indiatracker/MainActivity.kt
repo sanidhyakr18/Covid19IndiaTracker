866c0a59c7cc5e8a69892e1d07ed13fcf1a5c47d
sanidhyakr18 <sanidhyakr18@gmail.com>
2021-01-14 05:25:49 +0530

    added client and response

--- app/src/main/java/com/sandystudios/covid_19indiatracker/MainActivity.kt
+++ app/src/main/java/com/sandystudios/covid_19indiatracker/MainActivity.kt
@@ -2,10 +2,27 @@ package com.sandystudios.covid_19indiatracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.gson.Gson
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fetchResults()
+    }
+
+    private fun fetchResults() {
+        GlobalScope.launch {
+            val response = withContext(Dispatchers.IO) { Client.api.execute() }
+            if(response.isSuccessful) {
+                val data = Gson().fromJson(response.body?.string(), Response::class.java)
+            }
+        }
     }
 }
