1b49f9e19d8d73ae4c7f406f2bb3003717105456
sanidhyakr18 <sanidhyakr18@gmail.com>
2021-01-14 06:05:42 +0530

    fetch result in top container

--- app/src/main/java/com/sandystudios/covid_19indiatracker/MainActivity.kt
+++ app/src/main/java/com/sandystudios/covid_19indiatracker/MainActivity.kt
@@ -3,25 +3,74 @@ package com.sandystudios.covid_19indiatracker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.text.NumberFormat
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.statusBarColor = resources.getColor(R.color.white)
+        val view: View = window.decorView
+        view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
 
         fetchResults()
     }
 
     private fun fetchResults() {
         GlobalScope.launch {
-            val response = withContext(Dispatchers.IO) { Client.api.execute() }
-            if(response.isSuccessful) {
+            val response = withContext(Dispatchers.IO) { Client.api.clone().execute() }
+            if (response.isSuccessful) {
+                swipeToRefresh.isRefreshing = false
                 val data = Gson().fromJson(response.body?.string(), Response::class.java)
+                launch(Dispatchers.Main) {
+                    bindCombinedData(data.statewise[0])
+//                    bindStateWiseData(data.statewise.subList(0, data.statewise.size))
+                }
+            }
+        }
+    }
+
+    private fun bindCombinedData(data: StatewiseItem) {
+        val lastUpdatedTime = data.lastupdatedtime
+        val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
+        lastUpdatedTv.text = "Last Updated\n ${getTimeAgo(
+            simpleDateFormat.parse(lastUpdatedTime)
+        )}"
+
+        confirmedTv.text = NumberFormat.getIntegerInstance().format(Integer.valueOf(data.confirmed))
+        activeTv.text = NumberFormat.getIntegerInstance().format(Integer.valueOf(data.active))
+        recoveredTv.text = NumberFormat.getIntegerInstance().format(Integer.valueOf(data.recovered))
+        deceasedTv.text = NumberFormat.getIntegerInstance().format(Integer.valueOf(data.deaths))
+    }
+
+    fun getTimeAgo(past: Date): String {
+        val now = Date()
+        val seconds = TimeUnit.MILLISECONDS.toSeconds(now.time - past.time)
+        val minutes = TimeUnit.MILLISECONDS.toMinutes(now.time - past.time)
+        val hours = TimeUnit.MILLISECONDS.toHours(now.time - past.time)
+
+        return when {
+            seconds < 60 -> {
+                "Few seconds ago"
+            }
+            minutes < 60 -> {
+                "$minutes minutes ago"
+            }
+            hours < 24 -> {
+                "$hours hour ${minutes % 60} min ago"
+            }
+            else -> {
+                SimpleDateFormat("dd/MM/yy, hh:mm a").format(past).toString()
             }
         }
     }
